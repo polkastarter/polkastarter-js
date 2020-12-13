9111f2a26a9fe16b53a10d0ff0595e18aabfe387
Rui Pedro <ruipedro@MacBook-Pro.Home>
2020-12-13 17:52:55 +0000

    update

--- tests/index.js
+++ tests/index.js
@@ -10,8 +10,8 @@ var contractAddress = '0x420751cdeb28679d8e336f2b4d1fc61df7439b5a';
 var userPrivateKey = '0x7f76de05082c4d578219ca35a905f8debe922f1f00b99315ebf0706afc97f132';
 
 const expect = chai.expect;
-const tokenPurchaseAmount = 60;
-const tokenFundAmount = 100;
+const tokenPurchaseAmount = 0.0000000000021455460;
+const tokenFundAmount = 0.000000000003455460;
 const tradeValue = 0.0000000000012342;
 
 context('Tests', async () => {
@@ -59,28 +59,27 @@ context('Tests', async () => {
 
     it('GET - tokensAllocated', mochaAsync(async () => {        
         let tokens = await swapContract.tokensAllocated();
-        expect(parseFloat(tokens)).to.equal(0);
+        expect(tokens).to.equal(Number(0).noExponents());
     }));
 
-     
     it('GET - tradeValue', mochaAsync(async () => {        
         let td = await swapContract.tradeValue();
-        expect(parseFloat(td)).to.equal(tradeValue);
+        expect(td).to.equal(Number(tradeValue).noExponents());
     }));
 
     it('GET - tokensAvailable', mochaAsync(async () => {        
         let tokens = await swapContract.tokensAvailable();
-        expect(parseFloat(tokens)).to.equal(0);
+        expect(tokens).to.equal(Number(0).noExponents());
     }));
 
     it('GET - tokensForSale', mochaAsync(async () => {        
         let tokens = await swapContract.tokensForSale();
-        expect(parseFloat(tokens)).to.equal(100);
+        expect(Number(tokens).noExponents()).to.equal(Number(tokenFundAmount).noExponents());
     }));
 
     it('GET - tokensLeft', mochaAsync(async () => {        
         let tokens = await swapContract.tokensLeft();
-        expect(parseFloat(tokens)).to.equal(100);
+        expect(Number(tokens).noExponents()).to.equal(Number(tokenFundAmount).noExponents());
     }));
 
     it('should fund a Swap Contract and confirm balances', mochaAsync(async () => {
@@ -100,7 +99,7 @@ context('Tests', async () => {
 
     it('GET - tokensAvailable', mochaAsync(async () => {        
         let tokens = await swapContract.tokensAvailable();
-        expect(parseFloat(tokens)).to.equal(100);
+        expect(tokens).to.equal(Number(tokenFundAmount).noExponents());
     }));
 
     it('GET - isFunded', mochaAsync(async () => {  
@@ -121,7 +120,7 @@ context('Tests', async () => {
 
     it('GET - tokensAvailable after fund', mochaAsync(async () => {        
         let tokens = await swapContract.tokensAvailable();
-        expect(parseFloat(tokens)).to.equal(tokenFundAmount);
+        expect(tokens).to.equal(Number(tokens).noExponents());
     }));
 
     it('should do a non atomic swap on the Contract', mochaAsync(async () => {
@@ -144,7 +143,7 @@ context('Tests', async () => {
     it('GET - Purchase ID', mochaAsync(async () => {     
         let purchases = await swapContract.getAddressPurchaseIds({address : app.account.getAddress()}); 
         let purchase = await swapContract.getPurchase({purchase_id : purchases[0]}); 
-        expect(purchase.amount).to.equal(tokenPurchaseAmount);
+        expect(Number(purchase.amount).noExponents()).to.equal(Number(tokenPurchaseAmount).noExponents());
         expect(purchase.purchaser).to.equal(app.account.getAddress());
         expect(purchase.wasFinalized).to.equal(true);
         expect(purchase.reverted).to.equal(false);
@@ -153,7 +152,7 @@ context('Tests', async () => {
 
     it('GET - tokensAvailable after Swap', mochaAsync(async () => {        
         let tokens = await swapContract.tokensAvailable();
-        expect(tokens).to.equal(tokenFundAmount-tokenPurchaseAmount);
+        expect(Number(tokens).noExponents()).to.equal(Number(tokenFundAmount-tokenPurchaseAmount).noExponents());
     }));
 
     it('GET - Buyers', mochaAsync(async () => {  
