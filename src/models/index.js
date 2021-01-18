edf6207c91c1d9743b1d835e75f3d0b5e1b1b4ea
Rui Pedro <ruipedro@MacBook-Pro.Home>
2021-01-18 22:22:57 +0000

    web3 to ethereum

--- src/models/index.js
+++ src/models/index.js
@@ -49,7 +49,7 @@ class Application {
 		}
 		
 		if (typeof window !== "undefined") {
-			window.web3 = this.web3;
+			window.ethereum = this.web3;
 		}else{
 			if(!this.test){
 				throw new Error("Please Use an Ethereum Enabled Browser like Metamask or Coinbase Wallet");
@@ -63,8 +63,8 @@ class Application {
 		try{
 			if (typeof window === "undefined") { return false; }
 			if (window.ethereum) {
-				window.web3 = new Web3(window.ethereum);
-				this.web3 = window.web3;
+				window.ethereum = new Web3(window.ethereum);
+				this.web3 = window.ethereum;
 				await window.ethereum.enable();
 				return true;
 			}
